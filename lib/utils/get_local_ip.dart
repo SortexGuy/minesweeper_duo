1abb83687e23a7a671736e75d00f883f892692ea
Rubén A <sortexjuegos@gmail.com>
2025-07-21 13:52:53 -0400

    fix: Menos dependencias al obtener ip

--- lib/utils/get_local_ip.dart
+++ lib/utils/get_local_ip.dart
@@ -1,7 +1,22 @@
-import 'package:network_info_plus/network_info_plus.dart';
-  
+import 'dart:io';
+// import 'package:network_info_plus/network_info_plus.dart';
+
 Future<String> getLocalIp() async {
-  final info = NetworkInfo();
-  final ip = await info.getWifiIP();
-  return ip ?? '';
+  // final info = NetworkInfo();
+  // final ip = await info.getWifiIP();
+  var ip = '';
+  final interfaces = await NetworkInterface.list(
+    includeLoopback: false,
+    includeLinkLocal: false,
+  );
+  for (var interface in interfaces) {
+    for (var address in interface.addresses) {
+      if (address.type == InternetAddressType.IPv4) {
+        ip = address.address;
+      }
+    }
+  }
+  print("!!!!! Local IP: ${ip}");
+  return ip;
 }
+
